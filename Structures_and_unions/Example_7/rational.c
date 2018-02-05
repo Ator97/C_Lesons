0ff6972538d208da21242a8cdab17502a47033be
ator97 <oscargutcas9705@gmail.com>
2018-02-05 14:14:39 -0600

    Rational numbers are coming

---
+++ Structures_and_unions/Example_7/rational.c
@@ -0,0 +1,8 @@
+/*
+Author: Óscar Gutiérrez Castillo
+Program: rational
+Info: Support to rational numbers
+License: GNU General Public License v3.0
+Date: February 5, 2018 (Last change)
+TODO: 
+*/
